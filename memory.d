f622915893277117aad5a656da63300214f2f1bc
Robin Hübner <profan@pagefault.se>
2015-03-17 18:29:39 +0100

    Now sort of handles both classes and structs.

--- memory.d
+++ memory.d
@@ -1,17 +1,32 @@
 module profan.memory;
 
+import core.stdc.stdio : printf;
 import core.stdc.stdlib : malloc, free;
 import std.conv : emplace;
 import core.memory : GC;
 
-T allocate(T, Args...)(Args args) {
-	auto size = __traits(classInstanceSize, T);
+auto allocate(T, Args...)(Args args) {
+
+	size_t size;
+	static if (is(T == class)) {
+		size = __traits(classInstanceSize, T);
+	} else if (is(T == struct)) {
+		size = T.sizeof;
+	}
+
 	auto memory = malloc(size)[0..size];
+
+	if (!memory) {
+		printf("Memory allocation failed, tried to allocate %d bytes.", size);
+		return null;
+	}
+
 	GC.addRange(memory.ptr, size);
 	return emplace!(T, Args)(memory, args);
+
 }
 
-class Test {
+class ClassTest {
 
 	this(int v1, int v2) {
 		this.var1 = v1;
@@ -20,6 +35,13 @@ class Test {
 
 	int var1;
 	int var2;
+
+}
+
+struct StructTest {
+
+	int var1 = 0;
+	int var2 = 0;
 
 }
 
@@ -39,8 +61,13 @@ unittest {
 	
 	import std.stdio : writefln;
 
-	auto ptr = SmartPtr!Test(allocate!(Test)(10, 10));
-	writefln("Test var1: %d" , ptr.var1);
-	writefln("Test var2: %d" , ptr.var2);
+	auto ptr1 = SmartPtr!(ClassTest)(allocate!(ClassTest)(10, 10));
+	writefln("Test var1: %d" , ptr1.var1);
+	writefln("Test var2: %d" , ptr1.var2);
+
+	auto ptr2 = SmartPtr!(StructTest*)(allocate!(StructTest)());
+	writefln("Test var1: %d" , ptr2.var1);
+	writefln("Test var2: %d" , ptr2.var2);
+	
 
 }
